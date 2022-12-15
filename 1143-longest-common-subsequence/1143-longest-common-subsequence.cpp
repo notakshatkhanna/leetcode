24bea266406c5e58eafb3782859e87fbade90dcb
Akshat Khanna <byethost.panel@gmail.com>
2022-12-15 19:55:08 +0530

    Time: 34 ms (63.09%), Space: 10.8 MB (70.53%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int solve(int i, int j, string &s1, string &s2){
+        if(i==s1.length() or j==s2.length()){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i] == s2[j]){
+            return dp[i][j] = 1+solve(i+1, j+1, s1, s2);
+        }else{
+            return dp[i][j] = max(solve(i+1, j, s1, s2), solve(i, j+1, s1, s2));
+        }
+    }    
+    int longestCommonSubsequence(string text1, string text2) {
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0, text1, text2);
+    }
+};
