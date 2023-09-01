c41b7d551e0658634e698b75f98f9ca741b70bc2
Akshat Khanna <byethost.panel@gmail.com>
2023-09-02 01:55:33 +0530

    Time: 3 ms (89.66%), Space: 7.8 MB (89.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int solve(int i){
+    int solve(int i, vector<int> &dp){
         if(i==0)    return 0;
         if(i==1)    return 1;
-        return solve(i/2)+i%2;
+        if(dp[i])   return dp[i];
+        return dp[i] = solve(i/2,dp)+i%2;
     }
     
     vector<int> countBits(int n) {
-        vector<int> ans(n+1);
-        for(int i=0;i<=n;i++){
-            ans[i] = solve(i);
-        }
+        vector<int> ans(n+1,0);
+        for(int i=0;i<=n;i++)   ans[i] = solve(i, ans);
         return ans;
     }
 };
