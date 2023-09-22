a1be38512e696d0b041883696037aeaa1fe3a3d7
Akshat Khanna <byethost.panel@gmail.com>
2023-09-22 10:20:19 +0530

    Time: 3 ms (49.10%), Space: 6.7 MB (12.75%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -5,12 +5,8 @@ public:
         if(n>m) return false;
         int i=0, j=0;
         while(i<n and j<m){
-            if(s[i]==t[j]){
-                ++i;
-                ++j;
-            }
-            else    ++j;
-            if(i==n)    return true;
+            if(s[i]==t[j])  ++i;
+            ++j;
         }
         return i==n;
     }
